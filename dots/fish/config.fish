10b1a1a6b47d577a91f73b58615c98a76f0467d5
Hawtian Wang <twistoy.wang@gmail.com>
2023-04-15 17:26:55 +0800

    fix start ship

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -5,6 +5,7 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path /opt/homebrew/bin
 
 status is-interactive && zoxide init fish | source
+status is-interactive && starship init fish | source 
 
 eval (fnm env)
 
