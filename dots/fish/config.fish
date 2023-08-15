986487c96b9af0f4e9d237d84da3e5db8e2a57f0
Hawtian Wang <twistoy.wang@gmail.com>
2023-08-15 18:48:49 +0800

    fix

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -3,6 +3,8 @@ set fish_greeting
 fish_add_path $HOME/.fnm
 fish_add_path $HOME/.cargo/bin
 fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/opt/ruby/bin
+fish_add_path /opt/homebrew/lib/ruby/gems/3.2.0/bin/
 
 status is-interactive && zoxide init fish | source
 status is-interactive && starship init fish | source 
